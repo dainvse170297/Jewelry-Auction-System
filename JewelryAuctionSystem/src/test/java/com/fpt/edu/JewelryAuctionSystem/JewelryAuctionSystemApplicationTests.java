051bb4725e26fab734726cdc93f3172d0fa092c2
Nguyen Van Dai <dainvse170297@fpt.edu.vn>
2024-05-27 15:37:37 +0700

    fist_commit

---
+++ JewelryAuctionSystem/src/test/java/com/fpt/edu/JewelryAuctionSystem/JewelryAuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.edu.JewelryAuctionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryAuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
