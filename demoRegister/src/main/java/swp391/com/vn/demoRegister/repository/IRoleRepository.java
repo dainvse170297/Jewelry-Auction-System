c0780da956d3cbf79a6c22f71f251651320ef221
Nguyen Van Dai <dainvse170297@fpt.edu.vn>
2024-05-28 18:22:32 +0700

    Delete demoRegister directory

--- demoRegister/src/main/java/swp391/com/vn/demoRegister/repository/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package swp391.com.vn.demoRegister.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import swp391.com.vn.demoRegister.pojo.Role;
-
-public interface IRoleRepository extends JpaRepository<Role,Integer> {
-    // tìm role bởi name
-    Role findByName(String name);
-}
