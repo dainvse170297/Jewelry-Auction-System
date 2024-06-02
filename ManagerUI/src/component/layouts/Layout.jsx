acbaf5d69fb7bb72f040012067f0f93292f9bee9
Nguyen Van Dai <dainvse170297@fpt.edu.vn>
2024-06-02 23:17:30 +0700

    Update layout

---
+++ ManagerUI/src/component/layouts/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+
+function Layout() {
+  return ( 
+    <div>
+        <Container  fluid>
+            
+        </Container>
+      <h1>Header</h1>
+    </div>
+  );
+}
+
+export default Layout;
