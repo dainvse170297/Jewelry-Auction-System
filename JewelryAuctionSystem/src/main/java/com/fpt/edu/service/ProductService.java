2ed4cd33cdb2848f0057ee5d26ebc081d533bfe5
AnhNHVSE172136 <90208300+vietanh250303@users.noreply.github.com>
2024-06-03 16:45:36 +0700

    Update:ProductService
    Add product success but  cannot add product to Lot

--- JewelryAuctionSystem/src/main/java/com/fpt/edu/service/ProductService.java
+++ JewelryAuctionSystem/src/main/java/com/fpt/edu/service/ProductService.java
@@ -3,10 +3,13 @@ package com.fpt.edu.service;
 import com.fpt.edu.entity.Category;
 import com.fpt.edu.entity.Lot;
 import com.fpt.edu.entity.Product;
+import com.fpt.edu.entity.ValuationRequest;
 import com.fpt.edu.repository.CategoryRepository;
 import com.fpt.edu.repository.IProductRepository;
+import com.fpt.edu.repository.IValuationRequestRepository;
 import com.fpt.edu.repository.LotRepository;
 import com.fpt.edu.status.LotStatus;
+import com.fpt.edu.status.ValuationRequestStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,12 +23,17 @@ public class ProductService implements IProductService {
     private final IProductRepository productRepository;
     private final CategoryRepository categoryRepository;
     private final LotRepository lotRepository;
+    private final IValuationRequestRepository valuationRequestRepository;
 
 
     @Override
     public Product createProduct(int valuationRequestId, int categoryId, String name, String description, BigDecimal estimatePriceMax, BigDecimal estimatePriceMin) {
 
         Category category = categoryRepository.findById(categoryId).get();
+
+        ValuationRequest valuationRequest = valuationRequestRepository.findById(valuationRequestId).get();
+        valuationRequest.setValuationStatus(ValuationRequestStatus.PENDING_MANAGER_APPROVAL);
+
 
         Product product = new Product();
         product.setCategory(category);
@@ -35,11 +43,12 @@ public class ProductService implements IProductService {
         product.setEstimatePriceMin(estimatePriceMin);
         productRepository.save(product);
 
-        Lot lot = new Lot();
-        lot.setProduct(product);
-        lot.setStatus(LotStatus.WAITING);
-        lot.setAuctionSession(null);
-        lotRepository.save(lot);
+        valuationRequestRepository.save(valuationRequest);
+//        Lot lot = new Lot();
+//        lot.setProduct(product);
+//        lot.setStatus(LotStatus.WAITING);
+//        lot.setAuctionSession(null);
+//        lotRepository.save(lot);
 
         return product;
     }
