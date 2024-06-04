5bd070bba22a65f3c8f14dbf68805347a6f238c1
AnhNHVSE172136 <90208300+vietanh250303@users.noreply.github.com>
2024-06-04 17:10:49 +0700

    update:4/6/2024

--- JewelryAuctionSystem/src/main/java/com/fpt/edu/repository/AccountRepository.java
+++ JewelryAuctionSystem/src/main/java/com/fpt/edu/repository/AccountRepository.java
@@ -4,8 +4,10 @@ import com.fpt.edu.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 
     @Query(value = "SELECT acc FROM Account acc JOIN Staff st ON acc.staff.id = st.id")
-    Account findAccountByStaff();
+    List<Account> findAccountByStaff();
 }
