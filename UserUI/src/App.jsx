1034b5bb6f223018e1f3de2ae5d0935747f5ac49
datndse171888 <datndse171888@fpt.edu.vn>
2024-06-05 20:37:01 +0700

    20.36
    Add Fin_Valua_Req_List (Manager) + Mana_Approve_List (Staff) + Valua_Req (Member)
    Modify Router path on App.jsx

--- UserUI/src/App.jsx
+++ UserUI/src/App.jsx
@@ -5,22 +5,26 @@ import Register from './component/auth/register/Register.jsx';
 import CreateValuation from './component/valuation_request/create/CreateValuation.jsx';
 import Header from './component/layout/header/Header.jsx';
 import Selling from './component/selling/Selling.jsx';
+import ValuationRequest from './component/valuation_request/ValuationRequest.jsx';
+import ValuationRespone from './component/valuation_response/ValuationResponse.jsx';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-          <Header />
+        <Header />
 
-          <Routes>
-            <Route path='/' element={<Navigate to='/home' />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/create-valuation' element={<CreateValuation />} />
-            <Route path='/selling' element={<Selling />} />
-          </Routes>
+        <Routes>
+          <Route path='/' element={<Navigate to='/home' />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/create-valuation' element={<CreateValuation />} />
+          <Route path='/selling' element={<Selling />} />
+          <Route path='/valuation-request/:id' element={<ValuationRequest />} />
+          <Route path='/valuation-response/:id' element={<ValuationRespone />} />
+        </Routes>
 
       </BrowserRouter>
 
