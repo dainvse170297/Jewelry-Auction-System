b378a47ee75c18d0e62bb1c912a0e89c1fbb8bc4
AnhNHVSE172136 <90208300+vietanh250303@users.noreply.github.com>
2024-06-06 22:59:17 +0700

    update ui for addLotToSession

--- ManagerUI/src/component/manager/Session/AddLotToSession.jsx
+++ ManagerUI/src/component/manager/Session/AddLotToSession.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import '../../home/home.scss'
-import Sidebar from '../../layout/sidebar/Sidebar'
-import Navbar from '../../layout/navbar/Navbar'
-import { Link, useParams } from 'react-router-dom'
-import { FaBackward } from 'react-icons/fa'
 import axios from 'axios'
-import { Col } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Card, Carousel } from 'react-bootstrap'
+import { FaBackward } from 'react-icons/fa'
+import { Link, useParams } from 'react-router-dom'
+import '../../home/home.scss'
+import Navbar from '../../layout/navbar/Navbar'
+import Sidebar from '../../layout/sidebar/Sidebar'
 
 const AddLotToSession = () => {
 
     const [lot, setLot] = useState([])
     const { id } = useParams()
+
+    const [createdSessions, setCreatedSessions] = useState([])
 
     useEffect(() => {
         const getLotById = async () => {
@@ -25,7 +27,20 @@ const AddLotToSession = () => {
         getLotById()
     }, [id])
 
-    console.log(lot)
+    useEffect(() => {
+        const getAllCreatedSession = async () => {
+            try {
+                await axios.get('http://localhost:8080/auction/all-created-session').then((res) => {
+                    setCreatedSessions(res.data)
+                })
+            } catch (error) {
+                console.log("Error get all created session: ", error)
+            }
+        }
+        getAllCreatedSession()
+    }, [])
+
+    console.log(createdSessions)
 
     return (
         <div className='home'>
@@ -36,11 +51,57 @@ const AddLotToSession = () => {
                     <div className="mt-3">
                         <Link to={"/ready-lots"}><FaBackward /></Link>
                     </div>
-                    <h1 className='text-center mt-5'>Add Lot to Session</h1>
-                    <div className="">
-                        <h3>Lot Information</h3>
+                    <h1 className='text-center mt-3 mb-5'>Add Lot to Session</h1>
+                    <form action="">
+                        <div className="container">
+                            <div className="ms-5">
+                                <div className="row">
 
-                    </div>
+                                    <div className="col-lg-3">
+                                        <h3 className='text-center'>Lot Information</h3>
+                                        <Carousel>
+                                            {lot.product?.productImages?.map((image, index) => (
+
+                                                <Carousel.Item key={index}>
+                                                    <img
+                                                        className="d-block w-100"
+                                                        src={image.imageUrl}
+                                                        alt={lot.product.name + ' photo'}
+                                                        style={{ height: '300px', width: '200px' }}
+                                                    />
+                                                </Carousel.Item>
+
+                                            ))}
+                                        </Carousel>
+                                        <Card>
+                                            <Card.Body>
+                                                <Card.Title className='text-center'>{lot.product?.name}</Card.Title>
+                                                <Card.Text>
+                                                    <p>Estimate Max Price: <strong>{lot.product?.estimatePriceMax}</strong></p>
+                                                    <p>Estimate Min Price: <strong>{lot.product?.estimatePriceMin}</strong></p>
+                                                    <p>Current Price: <strong>{lot.currentPrice}</strong></p>
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </div>
+                                    <div className="col-lg-8">
+                                        <h3 className='text-center'>Sessions Information</h3>
+                                        <div className="">
+                                            <select name="" id="" className='form-select'>
+                                                <option value="" className='text-secondary'>--Select Session--</option>
+                                                {createdSessions.map((session, index) => (
+                                                    <option value={session.id} key={index}>
+                                                        <div className="">{session.name}</div>
+                                                    </option>
+                                                ))}
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
 
