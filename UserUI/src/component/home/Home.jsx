1ff9f0a3590b39fc76a11e8cc5860e4be3d80595
AnhNHVSE172136 <90208300+vietanh250303@users.noreply.github.com>
2024-06-10 00:58:23 +0700

    update user-ui add banner

--- UserUI/src/component/home/Home.jsx
+++ UserUI/src/component/home/Home.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import './home.scss';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import CreateValuation from '../valuation_request/create/CreateValuation';
+import './home.scss';
+import { Carousel } from 'react-bootstrap';
 
-const Home = () =>{
+const Home = () => {
     return (
         <>
-            <div className="home">
-                    <Routes>
-                        <Route path='/create-valuation' element={<CreateValuation />} />
-                    </Routes>
+            <div className="">
+                <Routes>
+                    <Route path='/create-valuation' element={<CreateValuation />} />
+                </Routes>
 
-                <p>Welcome to the Home Page</p>   
-            </div>     
-        </>                  
+                {/* <p>Welcome to the Home Page</p>    */}
+                <Carousel className='slide'>
+                    <Carousel.Item>
+                        {/* <ExampleCarouselImage text="First slide" /> */}
+                        <img src="https://t4.ftcdn.net/jpg/02/92/56/91/360_F_292569116_Phht4uRj1YIuLFgBhrLu8171npBOcJcr.jpg"
+                            alt=""
+                        />
+
+
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        {/* <ExampleCarouselImage text="Second slide" /> */}
+                        <img src="https://i.pinimg.com/originals/33/86/28/338628e1c2115db125fbfff2a36415a8.jpg"
+                            alt=""
+                        />
+
+
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        {/* <ExampleCarouselImage text="Second slide" /> */}
+                        <img src="https://zeevector.com/wp-content/uploads/Jewellery-Banner-Design-HD.png"
+                            alt=""
+                        />
+
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </>
     );
 }
 
