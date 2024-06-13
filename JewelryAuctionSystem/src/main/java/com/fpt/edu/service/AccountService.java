0386b1291a0f5061ac9fd6356c27e4c72b57acca
hailinh2910 <linhnhse170530@fpt.edu.vn>
2024-06-13 16:28:08 +0700

    update: vefiry token

--- JewelryAuctionSystem/src/main/java/com/fpt/edu/service/AccountService.java
+++ JewelryAuctionSystem/src/main/java/com/fpt/edu/service/AccountService.java
@@ -1,34 +1,40 @@
 package com.fpt.edu.service;
 
-import com.fpt.edu.dto.AccountDTO;
 import com.fpt.edu.entity.Account;
-import com.fpt.edu.mapper.AccountMapper;
 import com.fpt.edu.mapper.AuthenticationMapper;
 import com.fpt.edu.repository.IAccountRepository;
-import com.fpt.edu.response.AuthenticationResponse;
+import com.fpt.edu.security.request.IntrospectRequest;
+import com.fpt.edu.security.response.AuthenticationResponse;
+import com.fpt.edu.security.response.IntrospectResponse;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.NonFinal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AccountService implements IAccountService {
 
     private static final Logger log = LoggerFactory.getLogger(AccountService.class);
     private final IAccountRepository accountRepository;
 
     @NonFinal
-    protected static final String SIGNER_KEY =
-            "mGTSe7P+YzkdmH1KfIElpwED0hyOu6Mv4WZB/Qd54Jgvp4hUp73Tf06SndojREuL";
+    @Value("${jwt.signerKey}")
+    protected String SIGNER_KEY;
 
 
     @Override
@@ -38,14 +44,30 @@ public class AccountService implements IAccountService {
                 () -> new RuntimeException("Account not found")
         );
         String role = account.getRole().getName();
-             String token = generateToken(username,role);
+        Integer accountId = account.getId();
+        String token = generateToken(username,role,accountId);
 
             return AuthenticationMapper.toAuthenticationResponse(account, token);
 
     }
+    @Override
+    public IntrospectResponse introspect(IntrospectRequest introspectRequest) throws JOSEException, ParseException {
+        String token = introspectRequest.getToken();
+
+        JWSVerifier jwsVerifier = new MACVerifier(SIGNER_KEY.getBytes());
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date expirationTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        boolean verified =  signedJWT.verify(jwsVerifier);
+
+        return new IntrospectResponse(verified && expirationTime.after(new Date()));
 
 
-    private String generateToken(String username,String role) throws JOSEException {
+    }
+    
+    private String generateToken(String username,String role, Integer accountId) throws JOSEException {
         JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS512);
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                 .subject(username)
@@ -54,6 +76,7 @@ public class AccountService implements IAccountService {
                 .expirationTime(new Date(
                         Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
                 )).claim("role", role)
+                .claim("accountId",accountId)
                 .build();
 
         Payload payload = new Payload(jwtClaimsSet.toJSONObject());
--- JewelryAuctionSystem/src/main/java/com/fpt/edu/service/IAccountService.java
+++ JewelryAuctionSystem/src/main/java/com/fpt/edu/service/IAccountService.java
@@ -1,11 +1,14 @@
 package com.fpt.edu.service;
 
-import com.fpt.edu.dto.AccountDTO;
-import com.fpt.edu.response.AuthenticationResponse;
+import com.fpt.edu.security.request.IntrospectRequest;
+import com.fpt.edu.security.response.AuthenticationResponse;
+import com.fpt.edu.security.response.IntrospectResponse;
 import com.nimbusds.jose.JOSEException;
+
+import java.text.ParseException;
 
 public interface IAccountService {
 
     public AuthenticationResponse login(String username, String password) throws JOSEException;
-
+    public IntrospectResponse introspect(IntrospectRequest introspectRequest) throws JOSEException, ParseException;
 }
