0d49095217eb3ad8284b07065c63a41736ecc4ec
Nguyen Van Dai <dainvse170297@fpt.edu.vn>
2024-06-15 15:18:31 +0700

    Update: SRS report, upcoming session list

--- UserUI/src/component/auction-session/upcoming-session/UpcomingSession.jsx
+++ UserUI/src/component/auction-session/upcoming-session/UpcomingSession.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Countdown from "../../countdown/Countdown";
 import "./upcoming-session.scss";
+import { Spinner } from "react-bootstrap";
+import CountdownIcon from "@mui/icons-material/AccessAlarm";
 
 const UpcomingSession = () => {
   const [upcomingSessions, setUpcomingSessions] = useState([]);
@@ -35,23 +37,25 @@ const UpcomingSession = () => {
       <div className="row d-flex justify-content-center">
         <div className="col-10">
           {loading ? (
-            <span>Loading</span>
+            <Spinner animation="border" role="status">
+              {/* <span className="sr-only">Loading...</span> */}
+            </Spinner>
           ) : (
             upcomingSessions.map((session, index) => (
               <div className="row session-cart">
-                <div className="col-sm-4">
-                  <div className="row">
+                <div className="col-sm-4 d-flex justify-content-center align-items-center">
+                  {/* <div className="row">
                     <img
                       className="img-fluid"
                       src={session.defaultImageURL}
                       alt="..."
                     />
-                  </div>
-                  <div className="row d-flex justify-content-center">
+                  </div> */}
+                  {/* <div className="row d-flex justify-content-center">
                     <div className="col-sm-10 my-3">
                       <Countdown targetDate={session.startTime} />
                     </div>
-                  </div>
+                  </div> */}
                 </div>
                 <div className="col-sm-8 ">
                   <div className="row">
@@ -59,11 +63,25 @@ const UpcomingSession = () => {
                       <div className="row d-flex justify-content-center text-center">
                         <h3 className="py-3">{session.name}</h3>
                       </div>
-                      <div className="row d-flex justify-content-center text-center">
+                      <div className="row d-flex justify-content-center text-center mb-2">
+                        <div className="col-sm-6">
+                          <div className="countdown-label row p-2 border border-3 rounded-5">
+                            <div className="col-sm-2 d-flex justify-content-center align-items-center">
+                              <CountdownIcon
+                                style={{ width: "80%", height: "80%" }}
+                              />
+                            </div>
+                            <div className="col-sm-9">
+                              <Countdown targetDate={session.startTime} />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row d-flex justify-content-center text-center py-2">
                         <h5>{session.description}</h5>
                       </div>
                       <div className="row d-flex justify-content-center text-center">
-                        <h4 className="py-4">
+                        <h4 className="">
                           Time:
                           <strong>
                             {" "}
@@ -93,7 +111,11 @@ const UpcomingSession = () => {
                   </div>
                   <div className="row">
                     <div className="col-sm-4">
-                      <button className="">View Detail</button>
+                      <Link to={`/upcoming-session-detail/${session.id}`}>
+                        <button type="button" className="detail-button">
+                          View Detail
+                        </button>
+                      </Link>
                     </div>
                   </div>
                 </div>
--- UserUI/src/component/auction-session/upcoming-session/UpcomingSessionDetail.jsx
+++ UserUI/src/component/auction-session/upcoming-session/UpcomingSessionDetail.jsx
@@ -4,9 +4,13 @@ import React, { useEffect, useState } from "react";
 const UpcomingSessionDetail = () => {
   const { id } = useParams();
 
-  useEffect(() => {
-    // Fetch data from API
-  }, [id]);
+  // useEffect(() => {
+  //   const getAuctionSession = async () => {
+  //     try{
+  //       const params = new URLSearchParams();
+  //       params.append("memberId", localStorage.getItem("memberId");
+  //     }
+  // }, [id]);
   return (
     <>
       <div>this is{id}</div>
