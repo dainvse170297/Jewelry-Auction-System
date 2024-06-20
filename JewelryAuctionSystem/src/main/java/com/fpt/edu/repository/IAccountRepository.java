4dd4494a00e62a17a2b38e7c20fad6b86a326ade
hailinh2910 <linhnhse170530@fpt.edu.vn>
2024-06-21 00:44:11 +0700

    update: other core

--- JewelryAuctionSystem/src/main/java/com/fpt/edu/repository/IAccountRepository.java
+++ JewelryAuctionSystem/src/main/java/com/fpt/edu/repository/IAccountRepository.java
@@ -2,6 +2,7 @@ package com.fpt.edu.repository;
 
 import com.fpt.edu.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +10,7 @@ public interface IAccountRepository extends JpaRepository<Account, Integer> {
     public Optional<Account> findByUsernameAndPassword(String username, String password);
 
     Optional<Account> findByUsername(String username);
+
+
+
 }
