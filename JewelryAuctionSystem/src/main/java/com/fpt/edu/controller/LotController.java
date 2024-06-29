dca5639fefcadbb34fbe4d870cd7fa498d285a1e
Tran Quang Huy (K17 HCM) <91615324+W12Ardz@users.noreply.github.com>
2024-06-30 01:05:41 +0700

    fix lot ready

--- JewelryAuctionSystem/src/main/java/com/fpt/edu/controller/LotController.java
+++ JewelryAuctionSystem/src/main/java/com/fpt/edu/controller/LotController.java
@@ -2,6 +2,7 @@ package com.fpt.edu.controller;
 
 import com.fpt.edu.dto.LotDTO;
 import com.fpt.edu.entity.Lot;
+import com.fpt.edu.mapper.LotMapper;
 import com.fpt.edu.service.ILotService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +17,11 @@ import java.util.List;
 public class LotController {
 
     private final ILotService lotService;
+    private final LotMapper lotMapper;
 
     @GetMapping("/ready-lot")
-    public ResponseEntity<List<Lot>> getReadyLot() {
-        return ResponseEntity.ok(lotService.getLotsByStatusReady());
+    public ResponseEntity<List<LotDTO>> getReadyLot() {
+        return ResponseEntity.ok(lotMapper.toLotDTOS(lotService.getLotsByStatusReady()));
     }
 
     @GetMapping("/ready-lot/{id}")
@@ -42,4 +44,5 @@ public class LotController {
     public ResponseEntity<List<LotDTO>> getLotsByDeliveredAuctionRegister() {
         return ResponseEntity.ok(lotService.getLotsByDeliveredAuctionRegister());
     }
+
 }
