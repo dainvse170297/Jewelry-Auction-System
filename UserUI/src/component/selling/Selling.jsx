86b178da30303144db9c4bb50152ab888fe62fe7
hailinh2910 <linhnhse170530@fpt.edu.vn>
2024-07-02 01:42:09 +0700

    update:  User UI  About selling

--- UserUI/src/component/selling/Selling.jsx
+++ UserUI/src/component/selling/Selling.jsx
@@ -1,9 +1,255 @@
+import React from "react";
+import "./selling.scss"; // Import file SCSS đã tạo
+
 const Selling = () => {
-    return (
-        <div>
-            <h1>Selling</h1>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6">
+          <h1 className="text-center">Selling with FU AUCTION</h1>
+          <p>
+            FU AUCTION has been specifically designed to protect your interests
+            and maximize your results in the shortest possible timeframe. With
+            our unique model, we are at the forefront of revolutionizing the
+            industry, merging the tried-and-true methods of traditional auction
+            houses with the dynamism of modern innovative marketplace models,
+            utilizing cutting-edge technology and focusing on an unyielding
+            commitment to customer service.
+          </p>
+          <p>
+            With the lowest seller’s fees/premiums, very frequent (bi-weekly)
+            auctions, a substantial global following, record-breaking results,
+            and a flawless 5-star Google rating, FORTUNA stands head and
+            shoulders above the rest.
+          </p>
+          <div>
+            <p className="text-center slogan  md-5">
+              FU Auction is classy company that is laser focused on giving the
+              seller the best experience possible... they do exactly what they
+              say they are going to do.
+            </p>
+            <p className="text-center autho">
+              {" "}
+              <strong>Hai Linh</strong>{" "}
+              <p className="text-center author"> Team 6 </p>{" "}
+            </p>
+          </div>
         </div>
-    )
-}
+        <div className="col-md-6 text-center">
+          <h3>Ready to jump ahead and submit your valuation request?</h3>
+          <img
+            src="https://res.cloudinary.com/dhkmu458i/image/upload/v1718964404/slo1rvsyygsztnmqzpr4.jpg" // Đặt URL của ảnh vào đây
+            alt="Auction FU Image"
+            className="img-fluid rounded-circle" // Áp dụng class CSS từ Bootstrap để làm tròn và responsive
+            style={{ maxWidth: "320px", maxHeight: "320px" }} // Chỉnh kích thước ảnh
+          />
+          <p>
+            To jump ahead and submit a valuation request, click the button
+            below.
+          </p>
+          <a href="/create-valuation" className="btn btn-primary">
+            Request A Valuation
+          </a>
+          <h3>Or want to learn more first?</h3>
+          <p>
+            To continue learning about selling with FORTUNA, keep scrolling and
+            reading.
+          </p>
+        </div>
+
+        <div className="text-center md-5 my-5">
+          Now that you have an understanding of our mission and approach to
+          selling at FORTUNA, let’s delve deeper into the mechanics of our
+          operation, and explore the topics that every potential seller should
+          know. The following sections will highlight what makes FORTUNA
+          different and answer the question, “Why FORTUNA?”, as well as walk you
+          through the entire process from start to finish. This guide is
+          designed to prepare you thoroughly for your selling journey with
+          FORTUNA, giving you the confidence to make informed decisions at every
+          step of the way.
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="text-center md-5">Why Sell With FU AUCTION?</h1>
+          <div className="my-5 whysell">
+            <p>
+              At FORTUNA, we’re committed to setting and upholding the highest
+              standards for our sellers. Selling jewelry and watches can be a
+              challenging and confusing process, but it doesn’t have to be. With
+              FORTUNA by your side, you can rest assured that your valuable
+              assets are in the best hands possible.
+            </p>
+            <p>
+              We think of ourselves as a torch-bearer on the dimly-lit,
+              pitfall-filled path to selling jewelry and watches.
+            </p>
+            <p>
+              At FORTUNA, our core values of integrity, authenticity,
+              accountability, and excellence are at the forefront of everything
+              we do. We believe our values, performance, and reputation set us
+              far apart from our competitors.
+            </p>
+            <p>
+              Here are the five (5) reasons that answer the question, “Why Sell
+              with FORTUNA?”:
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="row my-5">
+        <div className="col-md-6">
+          <h1 className="text-center">
+            1 of 5: Our Goals Are Aligned With Yours
+          </h1>
+          <p>
+            FU AUCTION has been specifically designed to protect your interests
+            and maximize your results in the shortest possible timeframe. With
+            our unique model, we are at the forefront of revolutionizing the
+            industry, merging the tried-and-true methods of traditional auction
+            houses with the dynamism of modern innovative marketplace models,
+            utilizing cutting-edge technology and focusing on an unyielding
+            commitment to customer service.
+          </p>
+          <p>
+            With the lowest seller’s fees/premiums, very frequent (bi-weekly)
+            auctions, a substantial global following, record-breaking results,
+            and a flawless 5-star Google rating, FORTUNA stands head and
+            shoulders above the rest.
+          </p>
+        </div>
+        <div className="col-md-6 text-center">
+          <img
+            src="https://res.cloudinary.com/dhkmu458i/image/upload/v1719608039/zcjvpjtqu5oochipv87g.jpg" // Đặt URL của ảnh vào đây
+            alt="Auction FU Image"
+            className="img-fluid" // Áp dụng class CSS từ Bootstrap để làm tròn và responsive
+            style={{ maxWidth: "350px", maxHeight: "350px" }} // Chỉnh kích thước ảnh
+          />
+        </div>
+      </div>
+      <div className="row my-5">
+        <div className="col-md-6">
+          <h1 className="text-center">
+            2 of 5: We Have Solutions That Others Simply Do Not
+          </h1>
+          <p>
+            FU AUCTION has been specifically designed to protect your interests
+            and maximize your results in the shortest possible timeframe. With
+            our unique model, we are at the forefront of revolutionizing the
+            industry, merging the tried-and-true methods of traditional auction
+            houses with the dynamism of modern innovative marketplace models,
+            utilizing cutting-edge technology and focusing on an unyielding
+            commitment to customer service.
+          </p>
+          <p>
+            With the lowest seller’s fees/premiums, very frequent (bi-weekly)
+            auctions, a substantial global following, record-breaking results,
+            and a flawless 5-star Google rating, FORTUNA stands head and
+            shoulders above the rest.
+          </p>
+        </div>
+        <div className="col-md-6 text-center">
+          <img
+            src="https://res.cloudinary.com/dhkmu458i/image/upload/v1719619616/cknnas7ci037bxjhrei5.jpg" // Đặt URL của ảnh vào đây
+            alt="Auction FU Image"
+            className="img-fluid" // Áp dụng class CSS từ Bootstrap để làm tròn và responsive
+            style={{ maxWidth: "350px", maxHeight: "350px" }} // Chỉnh kích thước ảnh
+          />
+        </div>
+      </div>
+      <div className="row my-5">
+        <div className="col-md-6">
+          <h1 className="text-center">
+            3 of 5: We Are A Full-Service Specialty House, Not A Limited-Service
+            Everything House
+          </h1>
+          <p>
+            FU AUCTION has been specifically designed to protect your interests
+            and maximize your results in the shortest possible timeframe. With
+            our unique model, we are at the forefront of revolutionizing the
+            industry, merging the tried-and-true methods of traditional auction
+            houses with the dynamism of modern innovative marketplace models,
+            utilizing cutting-edge technology and focusing on an unyielding
+            commitment to customer service.
+          </p>
+          <p>
+            With the lowest seller’s fees/premiums, very frequent (bi-weekly)
+            auctions, a substantial global following, record-breaking results,
+            and a flawless 5-star Google rating, FORTUNA stands head and
+            shoulders above the rest.
+          </p>
+        </div>
+        <div className="col-md-6 text-center">
+          <img
+            src="https://res.cloudinary.com/dhkmu458i/image/upload/v1719859147/xoqwm1qwntkf9cqrp4cl.jpg" // Đặt URL của ảnh vào đây
+            alt="Auction FU Image"
+            className="img-fluid " // Áp dụng class CSS từ Bootstrap để làm tròn và responsive
+            style={{ maxWidth: "350px", maxHeight: "350px" }} // Chỉnh kích thước ảnh
+          />
+        </div>
+      </div>
+      <div className="row my-5">
+        <div className="col-md-6">
+          <h1 className="text-center">4 of 5: We Have A Global Following</h1>
+          <p>
+            FU AUCTION has been specifically designed to protect your interests
+            and maximize your results in the shortest possible timeframe. With
+            our unique model, we are at the forefront of revolutionizing the
+            industry, merging the tried-and-true methods of traditional auction
+            houses with the dynamism of modern innovative marketplace models,
+            utilizing cutting-edge technology and focusing on an unyielding
+            commitment to customer service.
+          </p>
+          <p>
+            With the lowest seller’s fees/premiums, very frequent (bi-weekly)
+            auctions, a substantial global following, record-breaking results,
+            and a flawless 5-star Google rating, FORTUNA stands head and
+            shoulders above the rest.
+          </p>
+        </div>
+        <div className="col-md-6 text-center">
+          <img
+            src="https://res.cloudinary.com/dhkmu458i/image/upload/v1719859177/wlrwva02polv8lcoyq9d.jpg" // Đặt URL của ảnh vào đây
+            alt="Auction FU Image"
+            className="img-fluid " // Áp dụng class CSS từ Bootstrap để làm tròn và responsive
+            style={{ maxWidth: "350px", maxHeight: "350px" }} // Chỉnh kích thước ảnh
+          />
+        </div>
+      </div>
+      <div className="row my-5">
+        <div className="col-md-6">
+          <h1 className="text-center">
+            5 of 5: We Deliver Top Results and Have an Unparalleled Reputation
+          </h1>
+          <p>
+            FU AUCTION has been specifically designed to protect your interests
+            and maximize your results in the shortest possible timeframe. With
+            our unique model, we are at the forefront of revolutionizing the
+            industry, merging the tried-and-true methods of traditional auction
+            houses with the dynamism of modern innovative marketplace models,
+            utilizing cutting-edge technology and focusing on an unyielding
+            commitment to customer service.
+          </p>
+          <p>
+            With the lowest seller’s fees/premiums, very frequent (bi-weekly)
+            auctions, a substantial global following, record-breaking results,
+            and a flawless 5-star Google rating, FORTUNA stands head and
+            shoulders above the rest.
+          </p>
+        </div>
+        <div className="col-md-6 text-center">
+          <img
+            src="https://res.cloudinary.com/dhkmu458i/image/upload/v1719859276/hpwj0xicsh9i7ktuhumy.jpg" // Đặt URL của ảnh vào đây
+            alt="Auction FU Image"
+            className="img-fluid " // Áp dụng class CSS từ Bootstrap để làm tròn và responsive
+            style={{ maxWidth: "350px", maxHeight: "350px" }} // Chỉnh kích thước ảnh
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Selling;
