3d7f5b170418dc7f600e75f9d31bdd9635c468b1
Tran Quang Huy (K17 HCM) <91615324+W12Ardz@users.noreply.github.com>
2024-07-05 01:38:59 +0700

    add: get notify by user

--- JewelryAuctionSystem/src/main/java/com/fpt/edu/repository/INotifyRepository.java
+++ JewelryAuctionSystem/src/main/java/com/fpt/edu/repository/INotifyRepository.java
@@ -1,10 +1,15 @@
 package com.fpt.edu.repository;
 
+import com.fpt.edu.entity.Member;
 import com.fpt.edu.entity.Notify;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface INotifyRepository extends JpaRepository<Notify, Integer> {
+    List<Notify> findByMemberId(Integer id);
+
+    List<Notify> findByMember(Member member);
 }
--- JewelryAuctionSystem/src/main/java/com/fpt/edu/service/INotifyService.java
+++ JewelryAuctionSystem/src/main/java/com/fpt/edu/service/INotifyService.java
@@ -3,7 +3,11 @@ package com.fpt.edu.service;
 import com.fpt.edu.dto.NotifyDTO;
 import com.fpt.edu.entity.Member;
 
+import java.util.List;
+
 public interface INotifyService {
     public NotifyDTO insertNotify(Member member, String title, String description);
     public void deleteNotify(Integer notifyId);
+
+    List<NotifyDTO> getNotifyByMemberId(Integer id);
 }
