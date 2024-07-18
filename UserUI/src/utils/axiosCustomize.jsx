fd290ace321d0110b09453d8103b91379f4ca5c6
Tran Quang Huy (K17 HCM) <91615324+W12Ardz@users.noreply.github.com>
2024-07-18 21:34:25 +0700

    Update axiosCustomize.jsx

--- UserUI/src/utils/axiosCustomize.jsx
+++ UserUI/src/utils/axiosCustomize.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:8080/",
-  baseURL: "https://jewelry-auction-system.azurewebsites.net/",
+  baseURL: "https://fuja.azurewebsites.net/",
 });
 
 // Add a request interceptor
